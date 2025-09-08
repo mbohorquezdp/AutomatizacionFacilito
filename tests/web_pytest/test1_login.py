125991837a9aea4bb43d8986a322e51a87084467
AndresCM <acisnerosm@outlook.com>
2025-09-08 18:34:13 -0500

    Corrección del test1

--- tests/web_pytest/test1_login.py
+++ tests/web_pytest/test1_login.py
@@ -12,6 +12,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Cargar variables de entorno
+load_dotenv()
+
 correo_cliente_antiguo = os.getenv("correo_ant")
 password_cliente_antiguo = os.getenv("password_ant")
 
@@ -19,8 +22,9 @@ password_cliente_antiguo = os.getenv("password_ant")
 def test_ingreso_web(driver):
     login = LoginPage(driver)
     login.load()
+    sleep(5)
     login.login_as_user(correo_cliente_antiguo, password_cliente_antiguo)
-    sleep(2)
+    sleep(5)
 
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Go to Home']")))
 
